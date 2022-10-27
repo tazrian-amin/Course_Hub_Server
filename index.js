4a36c49d01390580179386e21b4acae1850c842b
tazrian-amin <amin.tazrian1979@gmail.com>
2022-10-27 09:26:28 +0600

    updated index.js file

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Custom API Running');
+});
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const categoryCourses = course.filter(n => n.category_id === id);
+    res.send(categoryCourses);
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(n => n._id === id);
+    res.send(selectedCourse);
+});
+
+app.listen(port, () => {
+    console.log('Server running on port: ', port);
+});
